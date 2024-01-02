c4574d7a5bfe896710ee2a99c2bd2785cfb787f0
Quetzal <quetzaluzcategui@gmail.com>
2024-01-02 06:31:27 -0500

    practica1 created

--- PythonFundamentals/fundamentals.py
+++ PythonFundamentals/fundamentals.py
@@ -1,5 +1,7 @@
 print ("Hola Mundo")
 
+# input se usa para capturar datos, se debe declarar primero la variable donde sera almacenado
+
 numberInput = input("Introduce un numero")
 
 # Ejecuta el numero que introduce el usuario

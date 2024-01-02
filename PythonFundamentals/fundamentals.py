3778ae69fc30f4205609dc92d4bf7c129aa4eb4f
Quetzal <quetzaluzcategui@gmail.com>
2024-01-02 06:39:41 -0500

    Update fundamentals.py

--- PythonFundamentals/fundamentals.py
+++ PythonFundamentals/fundamentals.py
@@ -2,10 +2,10 @@ print ("Hola Mundo")
 
 # input se usa para capturar datos, se debe declarar primero la variable donde sera almacenado
 
-numberInput = input("Introduce un numero")
+numberInput = input("Introduce un numero cualquiera ")
 
 # Ejecuta el numero que introduce el usuario
 print ("El numero que ingresaste fue:", numberInput)
 
-numberInputINT = int(input("Introduce un numero"))
+numberInputINT = int(input("Introduce un numero entero "))
 print ("El numero entero que ingresaste fue: ", numberInputINT)

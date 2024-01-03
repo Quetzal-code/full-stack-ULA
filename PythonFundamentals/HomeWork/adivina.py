87a784301fb632350c457dc30af1a3f34598810f
Quetzal <quetzaluzcategui@gmail.com>
2024-01-03 08:36:06 -0500

    Update adivina.py

---
+++ PythonFundamentals/HomeWork/adivina.py
@@ -0,0 +1,9 @@
+#Crear un programa de python que simule un juego de adivinanzas
+
+#Debe crear un número aleatorio entre el 1 y el 20
+
+#el usuario tendra 5 intentos, y se le debe indicar como pista si es mas o menos del numero ingresado
+
+#al adivinar se le debe felicitar 
+
+#caso contrario de fallar los 5 intentos, mostrar el numero y un mensaje de animo

500f0be480ed5a60ce832de14cd63a16128e243c
Quetzal <quetzaluzcategui@gmail.com>
2024-01-03 07:47:56 -0500

    Update while.py

--- PythonFundamentals/while.py
+++ PythonFundamentals/while.py
@@ -2,3 +2,18 @@ contador = 0
 while contador < 5 :
     print(contador)
     contador += 1 #asegura que el bucle termine
+    # contador = contador + 1  seria lo mismo 
+
+numero = 15
+while numero <= 200 : 
+    print (numero)
+    numero += 1
+    if numero == 150 :
+        break
+
+    #continue
+    #sirve para excepciones, saltarse un valor
+    for i in range(3):
+        if i == 2 :
+            continue
+        print(i)

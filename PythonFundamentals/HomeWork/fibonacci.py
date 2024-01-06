c92793314c163adaafcbc81ca497c0cd13f6f48b
Quetzal <quetzaluzcategui@gmail.com>
2024-01-05 19:57:15 -0500

    summary

--- PythonFundamentals/HomeWork/fibonacci.py
+++ PythonFundamentals/HomeWork/fibonacci.py
@@ -1,1 +1,14 @@
 #
+
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+# Prueba de la función
+num_terminos = 10  # Por ejemplo, para los primeros 10 términos
+for i in range(num_terminos):
+    print(fibonacci(i))

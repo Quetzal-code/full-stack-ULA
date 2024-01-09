7dc0eede8c1d05a158974f8938e61fbd618dba54
Quetzal <quetzaluzcategui@gmail.com>
2024-01-08 20:05:14 -0500

    Create bi_array.py

---
+++ PythonFundamentals/Classes/Arrays/bi_array.py
@@ -0,0 +1,13 @@
+"""Bidimensional"""
+
+arregloBi = [[7,9,9,9],
+            [5,7,8,8],
+            [4,5,3,2],
+            [3,7,9,2]
+]
+SUMA= 0
+for fila in arregloBi:
+    for elemento in fila:
+        SUMA += elemento
+
+print("La suma total es: ", SUMA)

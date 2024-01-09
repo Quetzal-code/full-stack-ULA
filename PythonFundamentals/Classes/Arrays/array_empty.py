97b686fbb6dba3295a1e6eb8399b731ba28c07a8
Quetzal <quetzaluzcategui@gmail.com>
2024-01-08 19:00:30 -0500

    Update array_empty.py

--- PythonFundamentals/Classes/Arrays/array_empty.py
+++ PythonFundamentals/Classes/Arrays/array_empty.py
@@ -12,11 +12,11 @@ while True :
         lista_numeros.append(numero)
     except ValueError :
     
-        print("Introduce otro numero")
+        print("Introduce otro numero: ")
 
 if lista_numeros:
     suma=sum(lista_numeros)
     print("La suma es : ")
 
 else:
-    print("No hay datos")
+    print("No hay datos ")

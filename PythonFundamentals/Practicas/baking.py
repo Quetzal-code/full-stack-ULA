15ee2e2028094ce82f15f2c8dcf9181d60e57051
Quetzal <quetzaluzcategui@gmail.com>
2024-01-12 17:49:53 -0500

    Create baking.py

---
+++ PythonFundamentals/Practicas/baking.py
@@ -0,0 +1,17 @@
+EXPECTED_BAKE_TIME = 10
+
+def bake_time_remaining():
+        """Calculate the bake time remaining.
+
+    :param elapsed_bake_time: int - baking time already elapsed.
+    :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+    
+
+
+def elapsed_time_in_minutes():
+        return 10

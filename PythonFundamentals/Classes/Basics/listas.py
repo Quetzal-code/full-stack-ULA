f7a3d9fa7dd1a0a053c2a11f2c0c9355d9b77afb
Quetzal <quetzaluzcategui@gmail.com>
2024-01-16 19:15:17 -0500

    Update listas.py

--- PythonFundamentals/Classes/Basics/listas.py
+++ PythonFundamentals/Classes/Basics/listas.py
@@ -1,7 +1,14 @@
-deff 
+def bubble_sort(arreglo) :
+    n=len(arreglo)
+
+    for i in range(n): [
+        for j in range (0, n-i-1):
+            if arreglo [j] > arreglo [j+1]:
+            arreglo[j] , arreglo [j + 1] = arreglo [j + 1], arreglo [j]
+    ]
 
 
-deff prueba (y):
+def prueba (y):
     return y +1
 
 x = 5 

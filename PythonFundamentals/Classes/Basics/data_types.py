f2d0e504f14eea7694fbe22fa8572af05bee2ed9
Quetzal <quetzaluzcategui@gmail.com>
2024-01-18 14:53:13 -0500

    Update data_types.py

--- PythonFundamentals/Classes/Basics/data_types.py
+++ PythonFundamentals/Classes/Basics/data_types.py
@@ -29,3 +29,14 @@ x = txt[0]
 #
 txt = "Hello World"
 x = txt[2:5]
+
+
+txt = " Hello World "
+x = txt.strip()
+
+
+txt = "Hello World"
+txt = txt.upper()
+
+txt = "Hello World"
+txt = txt.replace("H", "J")

4c62bea2cb1dcfc625b81e0e008a698aabd08b08
Quetzal <quetzaluzcategui@gmail.com>
2024-01-18 14:49:56 -0500

    Update data_types.py

--- PythonFundamentals/Classes/Basics/data_types.py
+++ PythonFundamentals/Classes/Basics/data_types.py
@@ -1,4 +1,31 @@
+"""Data Types"""
+
 my_integer = 5 # Numeros enteros
 my_float = 5.0  # Números decimales
 my_string = "Hello"  # Texto y caracteres
 my_boolean = True # True or False
+
+#to convert
+x = 5
+x = float(x)
+
+
+x = 5
+x = complex(x)
+
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+
+#Para extraer la longitud de el string
+x = "Hello World"
+print(len(x))
+
+#Para tomar el primer caracter
+txt = "Hello World"
+x = txt[0]
+
+#
+txt = "Hello World"
+x = txt[2:5]

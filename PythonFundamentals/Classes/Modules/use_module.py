0c53716bab16e3ad479cedbfd86f72b4604d9719
Quetzal <80155867+Quetzal-code@users.noreply.github.com>
2024-01-18 16:20:06 -0500

    Update use_module.py

--- PythonFundamentals/Classes/Modules/use_module.py
+++ PythonFundamentals/Classes/Modules/use_module.py
@@ -1,4 +1,10 @@
-import module_example as me
+import module_example as me #Call the module and declare an alias
 me.greet("Alice")
 
 print("Hola Mundo")
+
+
+
+#To print everything inside a module
+# import mymodule
+# print(dir(mymodule))

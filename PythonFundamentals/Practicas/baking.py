433f9962b7499169dbbaad4f5d04aaa14b6ff368
Quetzal <80155867+Quetzal-code@users.noreply.github.com>
2024-01-20 22:03:55 -0500

    Update baking.py

--- PythonFundamentals/Practicas/baking.py
+++ PythonFundamentals/Practicas/baking.py
@@ -1,5 +1,5 @@
 """Baking calculator"""
-EXPECTED_BAKE_TIME = 60
+EXPECTED_BAKE_TIME = 40
 # Supongo que el tiempo de horneado esperado es de 60 minutos
 
 def bake_time_remaining(elapsed_bake_time):
@@ -11,50 +11,31 @@ def bake_time_remaining(elapsed_bake_time):
     """
     return EXPECTED_BAKE_TIME - elapsed_bake_time
 
-resultado = bake_time_remaining(30)
-print(f"Tiempo de horneado restante: {resultado} minutos")
+print(f"Tiempo de horneado restante: {bake_time_remaining(30)} minutos")
+
+PREPARATION_TIME = 10
+
+def preparation_time_in_minutes(number_of_layers):
+    """
+    Calcula el tiempo de preparación en minutos.
+    
+    :param layers: int - número de capas de la lasaña.
+    :return: int - tiempo total de preparación (en minutos).
+    """
+    return PREPARATION_TIME * number_of_layers
 
 
-# def calcular_y_mostrar_tiempo_restante(elapsed_bake_time):
-#     """
-#     Calcula y muestra el tiempo de horneado restante.
-#     """
-#     resultado = EXPECTED_BAKE_TIME - elapsed_bake_time
-#     print(f"Tiempo de horneado restante: {resultado} minutos")
-
-# # Llamar a la función
-# calcular_y_mostrar_tiempo_restante(30)
+print(f"Tiempo de horneado restante: {preparation_time_in_minutes(2)} minutos")
 
 
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    """
+    Calcula el tiempo total transcurrido en minutos.
+    
+    :param number_of_layers: int - número de capas de la lasaña.
+    :param elapsed_bake_time: int - tiempo de horneado ya transcurrido.
+    :return: int - tiempo total transcurrido (en minutos).
+    """
+    return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
 
-
-# def preparation_time_in_minutes(layers):
-#     """
-#     Calcula el tiempo de preparación en minutos.
-
-#     :param layers: int - número de capas de la lasaña.
-#     :return: int - tiempo total de preparación (en minutos).
-
-#     Esta función toma el número de capas de lasaña y devuelve el tiempo total
-#     de preparación, asumiendo un tiempo de preparación fijo por cada capa.
-#     """
-#     return layers * PREPARATION_TIME
-
-# def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
-#     """
-#     Calcula el tiempo total transcurrido en minutos.
-
-#     :param number_of_layers: int - número de capas de la lasaña.
-#     :param elapsed_bake_time: int - tiempo de horneado ya transcurrido.
-#     :return: int - tiempo total transcurrido (en minutos).
-
-#     Esta función toma el número de capas de lasaña y el tiempo de horneado ya
-#     transcurrido y devuelve el tiempo total transcurrido (preparación + horneado).
-#     """
-#     return preparation_time_in_minutes(3) + elapsed_bake_time
-
-
-# def bake_printer():
-#     return print (bake_time_remaining) 
-
-# bake_printer
+print(f"Tiempo de horneado restante: {elapsed_time_in_minutes(2, 30)} minutos")

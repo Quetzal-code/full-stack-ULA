7baea7182550d147e754039f8d9c2f0bc49570b5
Quetzal <80155867+Quetzal-code@users.noreply.github.com>
2024-01-22 08:55:26 -0500

    Update currency_exchange.py

---
+++ PythonFundamentals/Practicas/currency_exchange.py
@@ -0,0 +1,22 @@
+
+#budget : The amount of money you are planning to exchange.
+#exchange_rate : The amount of domestic currency equal to one unit of foreign currency.
+#
+def exchange_money(budget, exchange_rate):
+    
+
+#exchanging_value : Amount of money that is taken from the budget to be exchanged.
+def get_change(budget, exchanging_value):
+    
+
+
+def get_value_of_bills(denomination, number_of_bills):
+
+
+def get_number_of_bills()
+    
+
+def get_leftover_of_bills()
+    
+
+def exchangeable_value(budget, exchange_rate, spread, denomination): 

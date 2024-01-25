e50ae5daa7d5464219a020b880c5c5ad6ed75972
Quetzal <80155867+Quetzal-code@users.noreply.github.com>
2024-01-25 07:53:45 -0500

    Update for.py

--- PythonFundamentals/Classes/Control Structures/for.py
+++ PythonFundamentals/Classes/Control Structures/for.py
@@ -7,3 +7,30 @@ for i in range(7) : #imprime de 0 a 6
     print(i)
 else: 
     print("Se completo sin interrupciones ")    
+
+
+favorites = ['Creme Brulee', 'Apple Pie', 'Churros', 'Tiramisú', 'Chocolate Cake']
+
+for dessert in favorites:
+    if dessert == 'Pudding':
+        print('Yes one of my favorite desserts is', dessert)
+        break 
+    else:
+        print('No sorry, not a dessert on my list')
+
+
+#The pass statement allows you to run through the loop in its entirety and 
+#effectively ignore that the if condition has been satisfied.
+
+
+num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
+
+count = 0
+
+for x,num in enumerate(num_list):
+    count += 1
+    if num == 36:
+        print('Number found at ', x)
+        break
+
+print(count)

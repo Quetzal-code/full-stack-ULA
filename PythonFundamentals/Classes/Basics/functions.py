9e2d0137d405b432b521d897d5a18ca77aeb8f93
Quetzal <80155867+Quetzal-code@users.noreply.github.com>
2024-01-26 10:42:38 -0500

    Update functions.py

--- PythonFundamentals/Classes/Basics/functions.py
+++ PythonFundamentals/Classes/Basics/functions.py
@@ -13,21 +13,20 @@ Add_two_numbers(3, 4)
 
 # Inconsistent indentation in your code blocks will raise an error.
  def add_three_numbers_misformatted(number_one, number_two, number_three):
-     result = number_one + number_two + number_three   # This was indented by 4 spaces.
-    print(result)     #this was only indented by 3 spaces
+  result = number_one + number_two + number_three   # This was indented by 4 spaces.
+  print(result)     #this was only indented by 3 spaces
 
   File "<stdin>", line 3
-    print(result)
-    ^
-IndentationError: unindent does not match any outer indentation level
+  print(result)
+  
+  IndentationError: unindent does not match any outer indentation level
 
 # Function definition on first line.
 def add_two_numbers(number_one, number_two):
   result = number_one + number_two
   return result  # Returns the sum of the numbers.
 
->>> add_two_numbers(3, 4)
-7
+add_two_numbers(3, 4)
 
 # This function will return None.
 def add_two_numbers(number_one, number_two):

9c04a44ff00d67b8e8c8e92ddd62f2b274904d3e
Quetzal <80155867+Quetzal-code@users.noreply.github.com>
2024-01-27 10:16:07 -0500

    Update tuples.py

--- PythonFundamentals/Classes/Lists/tuples.py
+++ PythonFundamentals/Classes/Lists/tuples.py
@@ -1,2 +1,6 @@
 my_tuple = ("apple", "banana", "cherry")
 print(my_tuple[1])  # Accessing tuple elements
+
+print(my_tuple.index("cherry"))
+
+#The Tuples are inmutable, can not change after being declared 

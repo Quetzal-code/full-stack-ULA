4d5cf0fb56b53f73457e0d6eac4bc1a1792671c7
Quetzal <80155867+Quetzal-code@users.noreply.github.com>
2024-01-28 13:28:53 -0500

    Update dictionaries.py

--- PythonFundamentals/Classes/Lists/dictionaries.py
+++ PythonFundamentals/Classes/Lists/dictionaries.py
@@ -1,3 +1,15 @@
+""""""
+#Dicctionaries have a Key : Value
 my_dict = {"brand": "Ford", "model": "Mustang", "year": 1964}
 my_dict["year"] = 2020  # Updating value
 print(my_dict)
+
+rest_dict = {1: "CheeseCake" , 2 : "CornCake" , 3: "PumpkinCake"}
+
+print(rest_dict[3])
+
+rest_dict[2] = "Coconut Yogurt"
+
+print(rest_dict[2])
+
+del rest_dict[1] #Use it to delete items

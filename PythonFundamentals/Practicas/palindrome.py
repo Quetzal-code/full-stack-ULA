f90007a574720e77da7cfea4f0ac5f1bd8d03991
Quetzal <80155867+Quetzal-code@users.noreply.github.com>
2024-02-01 20:51:59 -0500

    Create palindrome.py

---
+++ PythonFundamentals/Practicas/palindrome.py
@@ -0,0 +1,12 @@
+#Algorithm to create a Palindrome
+
+def is_palindrome(str):
+    start_index = 0
+    end_index = len(str) - 1
+
+    for x in str:
+        if str[start_index] != str[end_index]:
+            return False
+    return True
+
+print(is_palindrome("racecar"))

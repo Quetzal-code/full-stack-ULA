5c5e7c0a7d4ebc6e3aafa99bb49d575c6a72591f
Quetzal <80155867+Quetzal-code@users.noreply.github.com>
2024-03-11 23:31:35 -0400

    Update Starting.js

---
+++ JavaScript/Starting.js
@@ -0,0 +1,14 @@
+// Variables declaration
+const numberOfLikes = 7
+//Could be numbers, strings, booleans , like in Python
+
+console.log(numberOfLikes)
+//This is the way to print 
+
+//We could store results in variables 
+
+const firstNumber = 2;
+const secondNumber = 8;
+const total = firstNumber + secondNumber ; 
+console.log(total)
+//REMEMBERRRR TO ADD THE ;

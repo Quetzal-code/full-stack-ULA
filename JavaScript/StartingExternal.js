493339b95bd0d896d0b07d444f9c2fa02b7f9240
Quetzal <80155867+Quetzal-code@users.noreply.github.com>
2024-03-14 20:29:01 -0400

    Create StartingExternal.js

---
+++ JavaScript/StartingExternal.js
@@ -0,0 +1,43 @@
+// JavaScript is the world's most popular programming language.
+// JavaScript is the programming language of the Web.
+
+// Writing into an HTML element, using innerHTML.
+// Writing into the HTML output using document.write().
+// Writing into an alert box, using window.alert().
+// Writing into the browser console, using console.log().
+
+// Variables declaration
+const numberOfLikes = 7
+//Could be numbers, strings, booleans , like in Python
+
+console.log(numberOfLikes)
+//This is the way to print (display the value in the screen or console) 
+
+//We could store results in variables 
+
+const firstNumber = 2;
+const secondNumber = 8;
+const total = firstNumber + secondNumber ; 
+console.log(total);
+//REMEMBERRRR TO ADD THE ;
+
+//Inequality operator 
+1 !== 2 ;
+
+//Equality operator
+1 === 1 ;
+
+// JavaScript Statements
+// Values, Operators, Expressions, Keywords, and Comments.
+
+//Data types in JavaScript 
+//Numbers , Strings , Booleans , Objects, Functions, Undefined(Declarada pero no asignada), Null (Ningun valor), Symbol 
+
+//Comments in JS
+/*Multiline
+Comment
+*/
+
+//Functions
+//in Python is with def
+// function verifDisponibilidad()

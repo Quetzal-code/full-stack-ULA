cc6001834ae615274928b5ee56f5a49dc2c0944f
Quetzal <80155867+Quetzal-code@users.noreply.github.com>
2024-03-19 23:34:52 -0400

    Update StartingExternal.js

--- JavaScript/StartingExternal.js
+++ JavaScript/StartingExternal.js
@@ -11,17 +11,17 @@ const numberOfLikes = 7
 //Could be numbers, strings, booleans , like in Python
 
 console.log(numberOfLikes)
-//This is the way to print (display the value in the screen or console) 
+//This is the way to print (display the value in the screen or console)
 
-//We could store results in variables 
+//We could store results in variables
 
 const firstNumber = 22;
 const secondNumber = 83;
-const total = firstNumber + secondNumber ; 
+const total = firstNumber + secondNumber ;
 console.log(total);
 //REMEMBERRRR TO ADD THE ;
 
-//Inequality operator 
+//Inequality operator
 1 !== 2 ;
 
 let neExample = true;
@@ -34,7 +34,7 @@ console.log(newNExample);
 // JavaScript Statements
 // Values, Operators, Expressions, Keywords, and Comments.
 
-//Data types in JavaScript 
+//Data types in JavaScript
 //Numbers , Strings , Booleans , Objects, Functions, Undefined(Declarada pero no asignada), Null (Ningun valor), Symbol 
 
 

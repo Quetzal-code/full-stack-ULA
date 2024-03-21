53ed4b9e02c8763c4e2d0fa8ebb9dd6f78e29cf2
Quetzal <80155867+Quetzal-code@users.noreply.github.com>
2024-03-20 21:21:20 -0400

    Update Boolean.js

--- JavaScript/Exercism/Boolean.js
+++ JavaScript/Exercism/Boolean.js
@@ -10,4 +10,13 @@ export function canFreePrisoner(
      return false
    } ;
   }
-  
+
+
+  export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
+    if(knightIsAwake===false && archerIsAwake===true && prisonerIsAwake===false || knightIsAwake===false && archerIsAwake===false && prisonerIsAwake===true || knightIsAwake===false && archerIsAwake===true && prisonerIsAwake===true || knightIsAwake===true && archerIsAwake===false && prisonerIsAwake===false || knightIsAwake===true && archerIsAwake===true && prisonerIsAwake===false ||knightIsAwake===true && archerIsAwake===true && prisonerIsAwake===true ||
+      knightIsAwake===true && archerIsAwake===false && prisonerIsAwake===true){
+      return true
+    } else {
+      return false
+    };
+  }

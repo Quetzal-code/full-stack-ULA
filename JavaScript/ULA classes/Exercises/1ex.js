15a101dfb7cb1a0bbfca5780ec7b7d53f7df9485
Quetzal <80155867+Quetzal-code@users.noreply.github.com>
2024-03-24 23:19:03 -0400

    Create 1ex.js

---
+++ JavaScript/ULA classes/Exercises/1ex.js
@@ -0,0 +1,12 @@
+// Display something in the console
+console.log("Hello World");
+
+//variables declaration
+
+let name = "Quetz";
+let age = 27 ;
+
+console.log(`My name is ${name} and my age is ${age}`);
+
+age = 26;
+console.log(`My real age is ${age}`);

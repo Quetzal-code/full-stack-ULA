9399ecc326c154b9410e64c4ebe87f9d7df44aa7
Quetzal <80155867+Quetzal-code@users.noreply.github.com>
2024-03-24 23:35:11 -0400

    Update 1ex.js

--- JavaScript/ULA classes/Exercises/1ex.js
+++ JavaScript/ULA classes/Exercises/1ex.js
@@ -10,3 +10,25 @@ console.log(`My name is ${name} and my age is ${age}`);
 
 age = 26;
 console.log(`My real age is ${age}`);
+
+//Part 2 , data types
+//Numbers
+let FNumber = 8;
+let BNumber = 19;
+let sumNumbers = BNumber + FNumber;
+console.log(sumNumbers);
+
+//Concatenar
+let string1= "First One ";
+let string2= "String Two";
+let addOneAndTwo= string1 + string2;
+console.log(addOneAndTwo)
+
+//Booleans
+let seeTrue = 1===1;
+console.log(seeTrue);
+
+//Undefine and null
+let experimentNull= null;
+console.log(experimentNull);
+// console.log(experimentUnd);
